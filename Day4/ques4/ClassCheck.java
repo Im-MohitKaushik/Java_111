7afe6bb5629d23769919859886ed53db93af4a4e
Mohit2907 <mohitkaushik2907@gmail.com>
2022-05-27 23:57:57 +0530

    Day4 done

--- Day4/ques4/ClassCheck.java
+++ Day4/ques4/ClassCheck.java
@@ -2,4 +2,21 @@ package ques4;
 
 public class ClassCheck {
 
+	public static void main(String[] args) {
+		ClassCheck c1 = new ClassCheck(); 
+		c1.checkNum(46);
+	}
+	
+	public void checkNum(int x) {
+		if(x<0) {
+			System.out.println("Input:"+ x + " ,Output:" + "Error");
+		}
+		if(x%2 == 1) {
+			System.out.println("Input:"+ x + " ,Output:" + x);
+		}
+		else if(x%2 == 0) {
+			int z = x/10;
+			System.out.println("Input:"+ x + " ,Output:" + (z+1)*10);
+		}
+	}
 }
